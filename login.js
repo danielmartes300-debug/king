6bee834832766630672a3d1cbb6113f40c80a17c
danielmartes300-debug <danielmartes300@gmail.com>
2026-02-19 06:51:21 -1100

    Update login.js

--- login.js
+++ login.js
@@ -1,22 +1,26 @@
 let intentos = 0;
 const maxIntentos = 4;
 
-document.getElementById("loginForm").addEventListener("submit", function(e){
-    e.preventDefault();
+const form = document.getElementById("loginForm");
+const mensaje = document.getElementById("mensajeLogin");
 
-    const mensaje = document.getElementById("mensajeLogin");
+form.addEventListener("submit", function(e){
+    e.preventDefault(); // IMPORTANTE
 
     intentos++;
+    console.log("Intento:", intentos); // para verificar en consola
 
-    if(intentos < maxIntentos){
+    if (intentos <= 3) {
         mensaje.style.color = "#c0392b";
         mensaje.textContent = "Contraseña incorrecta, intenta de nuevo";
-    } else {
+    } 
+    
+    if (intentos === 4) {
         mensaje.style.color = "#27ae60";
         mensaje.textContent = "Contraseña correcta";
-        
+
         setTimeout(() => {
             window.location.href = "token.html";
-        }, 1000); // espera 1 segundo antes de redirigir
+        }, 1000);
     }
 });
